91ce44f55ce2a58ea6111128dfd9401d693e053b
gigisoza <sozashviligigi@gmail.com>
2023-08-08 04:10:29 +0400

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import logo from "../images/logo-bookmark.svg";
+import menu from "../images/icon-hamburger.svg";
+import facebook from "../images/icon-facebook.svg";
+import twitter from "../images/icon-twitter.svg";
+
+export default function Header() {
+  return (
+    <>
+      <header className="flex items-center justify-between p-8">
+        <div>
+          <img src={logo} alt="" />
+        </div>
+
+        <nav className="hidden">
+          <ul>
+            <li>
+              <button>Features</button>
+            </li>
+            <li>
+              <button>Pricing</button>
+            </li>
+            <li>
+              <button>Contact</button>
+            </li>
+            <li>
+              <button>Login</button>
+            </li>
+          </ul>
+
+          <ul>
+            <li>
+              <img src={facebook} alt="" />
+            </li>
+            <li>
+              <img src={twitter} alt="" />
+            </li>
+          </ul>
+        </nav>
+
+        <div>
+          <button>
+            <img src={menu} alt="" />
+          </button>
+        </div>
+      </header>
+    </>
+  );
+}
