afefee37edf9c0e85df9f7524f8e4ede8c93c0f9
gigisoza <sozashviligigi@gmail.com>
2023-08-08 06:43:47 +0400

    Add Tabs.jsx file with functionality and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Showcase from "./components/Showcase";
+import Tabs from "./components/Tabs";
 
 function App() {
   return (
     <>
       <Header />
       <Showcase />
+      <Tabs />
     </>
   );
 }
